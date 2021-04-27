dfc70749d54f01da771699c9db837bd634b3dbcd
Tajper1 <davidebetlej1@gmail.com>
2021-04-28 01:44:38 +0200

    HTML Skeleton + todo list in script.js

---
+++ script.js
@@ -0,0 +1,3 @@
+// Functions TODO: Generate grid, set default grid value, set grid size, changing the color on hover, clearing the grid on button click
+
+const gridContainer = document.querySelector ('#grid-container')
