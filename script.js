e87585ae86e68a66273b146763a1509032418016
Tajper1 <davidebetlej1@gmail.com>
2021-04-28 03:12:58 +0200

    Finished the project, might add RGB option and more utility buttons

--- script.js
+++ script.js
@@ -2,23 +2,31 @@
 
 const gridContainer = document.querySelector ('#grid-container');
 const sizeButton = document.querySelector ('#sizeButton');
+
 sizeButton.addEventListener('click', changeSize);
+
 
 function defaultGrid () {
     generateGrid(16);
+    setGridSize(16);
 }
 
 function generateGrid (size) {
     for (let i = 0; i < size * size; i++) {
         const gridElement = document.createElement('div');
         gridElement.classList = 'gridElement';
+        gridElement.addEventListener('mouseover', colorHover);
         gridContainer.appendChild(gridElement);
     }
 }
 
+function setGridSize(size) {
+    gridContainer.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+  }
+
 function clearGrid () {
-    const gridArray = Array.from(gridContainer.childNodes)
-    gridArray.forEach(element => {
+    const gridTransformToArray = Array.from(gridContainer.childNodes)
+    gridTransformToArray.forEach(element => {
         gridContainer.removeChild(element);
     });
 }
@@ -31,5 +39,10 @@ function changeSize () {
     } else {
         clearGrid();
         generateGrid(mySize);
+        setGridSize(mySize);
     }
+}
+
+function colorHover(myDiv) {
+    myDiv.target.style.backgroundColor = `black`;
 }
